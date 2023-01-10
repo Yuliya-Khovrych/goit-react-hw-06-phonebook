33232afe1b0ac17617d124b060e4b59ac1096350
Yuliya-Khovrych <ulia.voitishenko@gmail.com>
2023-01-10 22:26:56 +0200

    corrected form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,39 +1,22 @@
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-//import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 import { nanoid } from 'nanoid';
 import { Form, Field, Input, Button } from './ContactForm.styled';
 import { addContact, getContact } from '../../redux/contactsSlice';
 
 export const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
   const dispatch = useDispatch();
   const contacts = useSelector(getContact);
-  console.log(contacts);
-
-  const handleChange = evt => {
-    const { name, value } = evt.target;
-
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-
-      case 'number':
-        setNumber(value);
-        break;
-
-      default:
-        return;
-    }
-  };
 
   const handleSubmit = evt => {
     evt.preventDefault();
-    dispatch(addContact({ name, number, id: nanoid() }));
-    setName('');
-    setNumber('');
+    const form = evt.target;
+    const newName = evt.target.name.value;
+    const number = evt.target.number.value;
+    contacts.find(({ name }) => name.toLowerCase() === newName.toLowerCase())
+      ? Notiflix.Notify.failure(`${newName} is already  in contacts.`)
+      : dispatch(addContact(newName, number));
+    form.reset();
   };
 
   return (
@@ -45,8 +28,6 @@ export const ContactForm = () => {
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          value={name}
-          onChange={handleChange}
           id={nanoid()}
           required
         />
@@ -58,8 +39,6 @@ export const ContactForm = () => {
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          value={number}
-          onChange={handleChange}
           id={nanoid()}
           required
         />
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,16 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState:  [],
+  initialState: [],
   reducers: {
-    addContact(state, action) {
-      state.push(action.payload);
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(name, number) {
+        return {
+          payload: {
+            name,
+            number,
+            id: nanoid(),
+          },
+        };
+      },
     },
     deleteContact(state, action) {
-      return state.filter(
-        contact => contact.id !== action.payload
-      );
+      return state.filter(contact => contact.id !== action.payload);
     },
   },
 });
